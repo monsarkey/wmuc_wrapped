6104b3dc504c5c1b286451b9d7fb831dcaafd579
monsarkey <00smarkey@gmail.com>
2022-11-18 02:34:30 -0500

    spotify authentication

--- main.py
+++ main.py
@@ -32,7 +32,24 @@ if __name__ == "__main__":
     
     # filter and re-index
     df = df[valid_channels]
-    df = df.reset_index()
-    df = df.drop(columns=['index'])
+    # df = df.reset_index()
+    # df = df.drop(columns=['index'])
 
-    print(df)
+    # for now, we are just using one example show for analysis but we could uncomment this.
+
+    # show_dfs = [x for _, x in df.groupby(by='show_title')]
+
+    # for show_df in show_dfs:
+    #     title = list(set(show_df.show_title))[0]
+    #     print(f"show {title} is of length {len(show_df)}")
+
+    example_df = df[df['show_title'] == "insufferable art house cinema soundtrack"]
+    example_df = example_df.reset_index()
+    example_df = example_df.drop(columns=['index'])
+
+    example_show = Show(example_df)
+    example_show.fill_spotify()
+
+    print(example_show)
+
+    # print(df)
--- show.py
+++ show.py
@@ -1,13 +1,38 @@
 import pandas as pd
 
+from spotipy.oauth2 import SpotifyOAuth
+from secrets import *
+
+scope = ""
 
 class Show:
 
-    def __init__(self):
-        self.data = None
+    def __init__(self, data: pd.DataFrame):
 
-    def load_df(self, data: pd.DataFrame):
-        self.data = data
+        self.token = None
+
+        self.title = data['show_title'].mode()[0]
+        self.dj_name = data['dj_name'].mode()[0]
+        self.dj_id = data['dj_id'].mode()[0]
+        self.channel = data['channel'].mode()[0]
+        self.df = data.drop(columns=['show_title', 'dj_id', 'dj_name', 'channel'])
+
+
+    def _get_token(self) -> str:
+        auth_manager = SpotifyOAuth(client_id=client_id, client_secret=client_secret, show_dialog=True, redirect_uri=redirect_uri, scope=None)
+        return auth_manager.get_access_token(as_dict=False)
+
+
+    def fill_spotify(self):
+        
+        if not self.token:
+            self.token = self._get_token()
+
+        print(self.token)
+
 
     def export(self):
         pass
+
+    def __str__(self) -> str:
+        return f"{self.title} on {self.channel}, presented by {self.dj_name}: {len(self.df)} total spins."
